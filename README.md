18295a5e9a6b8847ed44fd73793b02f82f349594
leinomk <38618113+leinomk@users.noreply.github.com>
2020-06-04 16:14:21 +0300

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phone-directory-app.herokuapp.com/
---
+++ build/precache-manifest.e3e9ea4dbac754ecc2d014f8549cd446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7a2b83ee288c6f9912a827892bce34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19acb69902140816815c",
+    "url": "/static/css/main.f2fcaf26.chunk.css"
+  },
+  {
+    "revision": "c96d6f703a6aff4b1fb1",
+    "url": "/static/js/2.40a526b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40a526b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19acb69902140816815c",
+    "url": "/static/js/main.350e11e3.chunk.js"
+  },
+  {
+    "revision": "72d410cc61ac6d893cd0",
+    "url": "/static/js/runtime-main.502d925a.js"
+  }
+]);
